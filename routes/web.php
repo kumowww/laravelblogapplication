20c7eca2bf3e7efe7f4d5d25f996db14724c4f18
Ernest Kavalski <150811468+kumowww@users.noreply.github.com>
2026-03-31 07:49:24 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "my site!";
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/post/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/post/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
